d37d5d6ee349ddc1fac1d27741d36d48d6938f01
chayusi <112993966+chayusi@users.noreply.github.com>
2024-01-18 10:03:47 +0800

    Add files via upload

---
+++ about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  imageUrl:string="assets/cha.jpg"; 
+imageW: number = 180;
+
+}
